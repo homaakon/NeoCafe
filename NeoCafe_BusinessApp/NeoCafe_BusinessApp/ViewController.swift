9f0dec240eb0410c7c76226b1a226ec5e86009e6
Igor Pak <ipak.tulane@gmail.com>
2024-02-05 13:41:10 +0500

    Add folder with BusinessApp

---
+++ NeoCafe_BusinessApp/NeoCafe_BusinessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoCafe_BusinessApp
+//
+//  Created by iPak Tulane on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
