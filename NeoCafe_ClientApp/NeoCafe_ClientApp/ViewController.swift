b531d10c4e483258405ea62e8426efa08417b9f2
Igor Pak <ipak.tulane@gmail.com>
2024-02-05 13:38:16 +0500

    Add folder with ClientApp

---
+++ NeoCafe_ClientApp/NeoCafe_ClientApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoCafe_ClientApp
+//
+//  Created by iPak Tulane on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
