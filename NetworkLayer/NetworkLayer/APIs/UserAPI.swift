98598bf6146b9b928b9438666fc7664a1ba49469
Igor Pak <ipak.tulane@gmail.com>
2024-02-06 22:02:24 +0500

    Delete a joint workspace; create instead two separate workspaces for each ClientApp and BizApp

--- NetworkLayer/NetworkLayer/APIs/UserAPI.swift
+++ NetworkLayer/NetworkLayer/APIs/UserAPI.swift
@@ -5,45 +5,45 @@
 //  Created by iPak Tulane on 06/02/24.
 //
 
-import Foundation
-import Moya
-
-enum UserAPI  {
-    case getUsers
-    // add your own cases
-}
-
-extension UserAPI: TargetType {
-    var baseURL: URL {
-        return URL(string: "https://659ed0e247ae28b0bd36c1e0.mockapi.io")!
-    }
-    
-    var path: String {
-        switch self {
-        case .getUsers:
-            "/siuzanna/api/users"
-        }
-    }
-    
-    var method: Moya.Method {
-        switch self {
-        case .getUsers:
-            return .get
-        }
-    }
-    
-    var task: Moya.Task {
-        switch self {
-        case .getUsers:
-            return .requestPlain
-        }
-    }
-    
-    var headers: [String : String]? {
-        switch self {
-        case .getUsers:
-            return .none
-        }
-    }
-}
-
+//import Foundation
+//import Moya
+//
+//enum UserAPI  {
+//    case getUsers
+//    // add your own cases
+//}
+//
+//extension UserAPI: TargetType {
+//    var baseURL: URL {
+//        return URL(string: "https://659ed0e247ae28b0bd36c1e0.mockapi.io")!
+//    }
+//    
+//    var path: String {
+//        switch self {
+//        case .getUsers:
+//            "/siuzanna/api/users"
+//        }
+//    }
+//    
+//    var method: Moya.Method {
+//        switch self {
+//        case .getUsers:
+//            return .get
+//        }
+//    }
+//    
+//    var task: Moya.Task {
+//        switch self {
+//        case .getUsers:
+//            return .requestPlain
+//        }
+//    }
+//    
+//    var headers: [String : String]? {
+//        switch self {
+//        case .getUsers:
+//            return .none
+//        }
+//    }
+//}
+//
